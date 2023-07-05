588e8a92967b96d2bede7213f8b6dd2b54ed994a
Matthew Gardner <1206132-gardnem6@users.noreply.gitlab.com>
2023-07-04 20:44:55 -0400

    update package base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/wt-passhelper/",
   plugins: [react()],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version)
